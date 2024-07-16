46aa3aecf76eb8f7da61e0352bdb7d1a95c5baf5
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-16 13:56:00 +0300

    Add project files.

---
+++ Q. Sequence of Numbers and Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int t = 0; t < 3; t++)
+{
+    int num1 = int.Parse(Console.ReadLine());
+    int num2 = int.Parse(Console.ReadLine());
+    int sum = 0;
+    if (num1 < num2 && num1 > 0 && num2 > 0)
+    {
+        for (int i = num1; i <= num2; i++)
+        {
+            sum += i;
+            Console.Write(i + " ");
+
+        }
+        Console.Write("sum = {0} ", sum);
+    }
+    else if (num1 > num2 && num1 > 0 && num2 > 0)
+    {
+        for (int i = num2; i <= num1; i++)
+        {
+            sum += i;
+            Console.Write(i + " ");
+
+        }
+        Console.Write("sum = {0} ", sum);
+    }
+    else if (num1 <= 0)
+    {
+        break;
+    }
+    else if (num2 <= 0)
+    {
+        break;
+    }
+    Console.WriteLine(" ");
+}
+Console.ReadLine();
+
